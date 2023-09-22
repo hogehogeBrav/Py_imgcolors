4165e0971ac1e886b7ebc236fbe027d4faf5d1db
hogehogeBrav <zmzmakoto@icloud.com>
2023-09-22 12:42:43 +0900

    def化

--- imgcolor.py
+++ imgcolor.py
@@ -39,7 +39,7 @@ def calculate_average_color(image_path):
 # 色の名前を取得（入力値に近い物）
 def closest_color(requested_color):
     min_colors = {}
-    for key, name in webcolors.CSS2_HEX_TO_NAMES.items():
+    for key, name in webcolors.CSS21_HEX_TO_NAMES.items():
         r_c, g_c, b_c = webcolors.hex_to_rgb(key)
         rd = (r_c - requested_color[0]) ** 2
         gd = (g_c - requested_color[1]) ** 2
@@ -47,31 +47,38 @@ def closest_color(requested_color):
         min_colors[(rd + gd + bd)] = name
     return min_colors[min(min_colors.keys())]
 
+# フォルダ内の画像ファイルを取得
+def get_image_files(folder_path):
+    return [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith(('.jpg', '.jpeg', '.png'))]
+
+# フォルダ内の画像の平均色を計算
+def imgcolor(folder_path):
+    # 画像の平均色を格納する配列
+    result = ""
+    count = 0
+    closest_colors = {}
+
+    # 各画像の平均色を計算し、配列に格納
+    for image_path in get_image_files(folder_path):
+        count += 1
+        average_color = calculate_average_color(image_path)
+
+        # 色が既に連想配列に存在するか確認
+        if closest_color(average_color) in closest_colors:
+            closest_colors[closest_color(average_color)] += 1
+        else:
+            closest_colors[closest_color(average_color)] = 1
+
+    # 色の出現回数を降順にソートして確率を計算
+    closest_colors = sorted(closest_colors.items(), key=lambda x:x[1], reverse=True)
+
+    for i in range(len(closest_colors)):
+        percent = round(closest_colors[i][1] / count * 100, 2)
+        color = closest_colors[i][0]
+        result = result + color + " " +  str(percent) + "%,"
+        # print(color + " " +  str(percent) + "%,", end="")
+    return result
+
 # 画像フォルダのパスを指定
 folder_path = './img'
-
-# フォルダ内の画像ファイルを取得
-image_files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith(('.jpg', '.jpeg', '.png'))]
-
-# 画像の平均色を格納する配列
-count = 0
-closest_colors = {}
-
-# 各画像の平均色を計算し、配列に格納
-for image_path in image_files:
-    count += 1
-    average_color = calculate_average_color(image_path)
-
-    # 色が既に連想配列に存在するか確認
-    if closest_color(average_color) in closest_colors:
-        closest_colors[closest_color(average_color)] += 1
-    else:
-        closest_colors[closest_color(average_color)] = 1
-
-# 色の出現回数を降順にソートして確率を計算
-closest_colors = sorted(closest_colors.items(), key=lambda x:x[1], reverse=True)
-for i in range(len(closest_colors)):
-    # 小数点第二位で四捨五入
-    closest_colors[i] = (closest_colors[i][0], round(closest_colors[i][1] / count, 2))
-
-print(closest_colors)
+print(imgcolor(folder_path))
